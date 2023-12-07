574008196d0208fbbe8af2ec8f6fffc7f44809c1
Wizzel1 <r_christian@gmx.de>
2023-12-07 18:44:03 +0100

    Init houdini

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://0zsqzifz.api.sanity.io/v2023-08-01/graphql/production/default"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
