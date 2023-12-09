2579c9cb00453c35ceaf55a66ea5d0bb968a288e
Wizzel1 <r_christian@gmx.de>
2023-12-09 10:19:22 +0100

    Export schemas

--- podcast-ohne-richtigen-namen/schemas/index.ts
+++ podcast-ohne-richtigen-namen/schemas/index.ts
@@ -1,6 +1,7 @@
 import blockContent from './blockContent'
-import category from './category'
 import post from './post'
 import author from './author'
+import episode from './episode'
+import host from './host'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, host, blockContent, episode]
