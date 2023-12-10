827d3f6d9a1ed77e24b15933630b6e9afa6cab9b
Wizzel1 <r_christian@gmx.de>
2023-12-10 16:22:24 +0100

    Latest changes

--- src/routes/episodes/+page.svelte
+++ src/routes/episodes/+page.svelte
@@ -5,15 +5,19 @@
 
 	$: ({ AllEpisodes } = data);
 	$: episodes = $AllEpisodes.data?.allEpisode ?? [];
+
+	$: console.log(episodes);
 </script>
 
 <ul role="list">
 	{#each episodes as item}
 		<li>
 			<a
-				class="block p-4 m-5 border border-solid border-stone-400 rounded-md hover:bg-sky-700 hover:cursor-pointer"
-				href="/episodes/{item.episodeNumber}">{item.episodeNumber}</a
+				class="block p-4 m-5 border border-solid border-stone-400 rounded-md hover:bg-sky-700 hover:cursor-pointer hover:no-underline"
+				href="/episodes/{item.episodeNumber}"
 			>
+				<p class="m-0">{item.episodeNumber}</p>
+			</a>
 		</li>
 	{/each}
 </ul>
--- src/routes/episodes/[id]/+page.svelte
+++ src/routes/episodes/[id]/+page.svelte
@@ -8,26 +8,27 @@
 </script>
 
 {#if item}
-	<div class="flex flex-col place-content-center my-8 sm:mx-5 mx-20">
-		<h3 class="block">{item.question}</h3>
+	<div class="flex justify-center">
+		<div class="flex flex-col max-w-2xl">
+			<h3>{item.question}</h3>
 
-		<p class="m-0">Antwort:</p>
-		<a href={item.answerLink} target="_blank" rel="noreferrer noopener">{item.answer}</a>
+			<p class="m-0">Antwort:</p>
+			<a class="w-fit" href={item.answerLink} target="_blank" rel="noreferrer noopener">{item.answer}</a>
 
-		{#if item.solvedBy}
-			<p>Gelöst von <a href="/hosts/{item.solvedBy._id}">{item.solvedBy.name}</a></p>
-		{:else}
-			<p>Gelöst von niemandem.</p>
-		{/if}
-
-		<iframe
-			title="Spotify Embed"
-			style="border-radius:12px"
-			src="https://open.spotify.com/embed/episode/{item.spotifyId}?utm_source=generator&theme=0"
-			width="50%"
-			height="152"
-			loading="lazy"
-			frameBorder="0"
-		></iframe>
+			{#if item.solvedBy}
+				<p>Gelöst von <a href="/hosts/{item.solvedBy._id}">{item.solvedBy.name}</a></p>
+			{:else}
+				<p>Gelöst von niemandem.</p>
+			{/if}
+			<iframe
+				title="Spotify Embed"
+				style="border-radius:12px"
+				src="https://open.spotify.com/embed/episode/{item.spotifyId}?utm_source=generator&theme=0"
+				width="100%"
+				height="152"
+				loading="lazy"
+				frameBorder="0"
+			></iframe>
+		</div>
 	</div>
 {/if}
