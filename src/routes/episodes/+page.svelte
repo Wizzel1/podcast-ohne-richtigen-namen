fd9f62ea8b104667ae19440751ec6228c7c77723
Wizzel1 <r_christian@gmx.de>
2023-12-10 11:29:34 +0100

    Update episodes list

--- src/routes/episodes/+page.svelte
+++ src/routes/episodes/+page.svelte
@@ -7,10 +7,13 @@
 	$: episodes = $AllEpisodes.data?.allEpisode ?? [];
 </script>
 
-<ul>
+<ul role="list">
 	{#each episodes as item}
 		<li>
-			<a href="/episodes/{item.episodeNumber}">{item.episodeNumber}</a>
+			<a
+				class="block p-4 m-5 border border-solid border-stone-400 rounded-md hover:bg-sky-700 hover:cursor-pointer"
+				href="/episodes/{item.episodeNumber}">{item.episodeNumber}</a
+			>
 		</li>
 	{/each}
 </ul>
