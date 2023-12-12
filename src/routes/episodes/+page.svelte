aaa4246a79f2dc5d494198701a2a0c2ae6be44ed
Wizzel1 <r_christian@gmx.de>
2023-12-12 14:23:09 +0100

    Fix episode chunking

--- src/routes/episodes/+page.svelte
+++ src/routes/episodes/+page.svelte
@@ -10,8 +10,9 @@
 
 	function chunkify(data: { episodeNumber: number | null }[]) {
 		const chunkedEpisodes = [];
-		for (let i = 0; i < data.length; i += 10) {
-			chunkedEpisodes.push(data.slice(i, i + 10));
+		for (let i = 1; i < data.length; i += 10) {
+			const index = i == 1 ? i - 1 : i;
+			chunkedEpisodes.push(data.slice(index, i + 10));
 		}
 		return chunkedEpisodes;
 	}
@@ -22,7 +23,7 @@
 </svelte:head>
 
 <div class="grid grid-cols-3 lg:grid-cols-4 gap-4 p-4 m-5">
-	{#each chunkedEpisodes as episodeList, index}
+	{#each chunkedEpisodes as episodeList}
 		{@const firstItem = episodeList[0]}
 		{@const lastItem = episodeList[episodeList.length - 1]}
 		<a class="w-full" href="/episodes/{firstItem.episodeNumber}-{lastItem.episodeNumber}">
