4e28ab3758b414d78ad19bde0cf4f70cd5eb6a64
Wizzel1 <r_christian@gmx.de>
2023-12-12 10:08:25 +0100

    Add episode range page

---
+++ src/routes/episodes/[start]-[end]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { PageData } from './$houdini';
+
+	export let data: PageData;
+
+	$: ({ EpisodeRange } = data);
+	$: episodes = $EpisodeRange.data?.allEpisode ?? [];
+</script>
+
+<ul role="list">
+	{#each episodes as item}
+		<li>
+			<a
+				class="block p-4 m-5 border border-solid border-stone-400 rounded-md hover:bg-sky-700 hover:cursor-pointer hover:no-underline"
+				href="/episodes/{item.episodeNumber}"
+			>
+				<p class="m-0">{item.episodeNumber}</p>
+                <p>{item.question}</p>
+			</a>
+		</li>
+	{/each}
+</ul>
