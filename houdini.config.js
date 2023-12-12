779ce62b269dbf4cd0bf6923d38313fcf67ecd97
jycouet <jycouet@gmail.com>
2023-12-12 20:56:19 +0100

    update queries

--- houdini.config.js
+++ houdini.config.js
@@ -2,12 +2,12 @@
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
-    "watchSchema": {
-        "url": "https://0zsqzifz.api.sanity.io/v2023-08-01/graphql/production/default"
-    },
-    "plugins": {
-        "houdini-svelte": {}
-    }
-}
+	watchSchema: {
+		url: 'https://0zsqzifz.api.sanity.io/v2023-08-01/graphql/production/default'
+	},
+	plugins: {
+		'houdini-svelte': {}
+	}
+};
 
-export default config
+export default config;
