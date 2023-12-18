fdfe80917a3980c5bccef192e36be689dcdafaf9
Wizzel1 <r_christian@gmx.de>
2023-12-18 10:45:35 +0100

    Update episode styling

--- src/routes/episodes/[id]/+page.svelte
+++ src/routes/episodes/[id]/+page.svelte
@@ -14,7 +14,7 @@
 			<a href="/episodes/{number - 1}">{'<'}</a>
 		</button>
 	</div>
-	<h2 class="text-3xl font-bold text-center">#{number}</h2>
+	<h2 class="text-3xl font-bold text-center">Folge #{number} {title}</h2>
 	<div>
 		<div>
 			<button>
@@ -29,18 +29,24 @@
 		{#if questions}
 			{#each questions as question, index}
 				<div class="flex flex-col p-5">
-					<p class="text-xl font-medium">Frage {index + 1}:</p>
-					<h3>{question?.question}</h3>
+					<p class="text-xl font-medium mb-3">Frage {index + 1}:</p>
+					<p class="mb-8">{question?.question}</p>
 
-					<p class="mt-10">Antwort:</p>
-					<a class="w-fit" href={question?.answerLink} target="_blank" rel="noreferrer noopener"
-						>{question?.answer}</a
+					<p class="text-xl font-medium mb-3">Antwort:</p>
+					<a
+						class="w-fit text-blue-400 mb-8"
+						href={question?.answerLink}
+						target="_blank"
+						rel="noreferrer noopener">{question?.answer}</a
 					>
 
-					<div class="mt-10">
+					<div class="">
+						<p class="text-xl font-medium mb-3">Ergebnis:</p>
 						{#if question?.solvedBy}
 							<p>
-								Gelöst von <a href="/hosts/{question?.solvedBy.name?.toLowerCase()}"
+								Gelöst von <a
+									class="text-blue-400"
+									href="/hosts/{question?.solvedBy.name?.toLowerCase()}"
 									>{question?.solvedBy.name}</a
 								>
 							</p>
@@ -58,8 +64,7 @@
 	<iframe
 		title="Spotify Embed"
 		style="border-radius:12px"
-		src="https://open.spotify.com/embed/{new URL(spotifyUrl)
-			.pathname}?utm_source=generator&theme=0"
+		src="https://open.spotify.com/embed/{new URL(spotifyUrl).pathname}?utm_source=generator&theme=0"
 		width="100%"
 		height="152"
 		loading="lazy"
