f6bd52b41837786114ac4e33d901fe8c921da2a4
Wizzel1 <r_christian@gmx.de>
2023-12-18 10:08:23 +0100

    Update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,10 @@
 		src="https://images.podigee-cdn.net/1400x,sPRLkYRRBW1cKURu4hNSAHa7eCfSJmk0ZX2oahz_yrGs=/https://main.podigee-cdn.net/uploads/u2838/ad6a8a9a-f913-4216-a093-8a9fee6e3d35.jpg"
 		alt=""
 	/>
-	<h1 class="text-xl font-semibold mx-2 my-4">Podcast ohne (richtigen) Namen</h1>
+	<h1 class="text-xl font-semibold mx-2 my-5">Podcast ohne (richtigen) Namen</h1>
 </div>
 
-<nav class="flex justify-around my-4">
+<nav class="flex justify-around my-5 mx-10">
 	<a href="/">Home</a>
 	<a href="/episodes">Episoden</a>
 	<a target="_blank" rel="noreferrer noopener" href="https://podcastohnerichtigennamen.de/shop/"
@@ -19,7 +19,7 @@
 	>
 </nav>
 
-<div class="mx-10 my-5">
+<div class="mx-2 md:mx-10 my-5">
 	<slot />
 </div>
 
