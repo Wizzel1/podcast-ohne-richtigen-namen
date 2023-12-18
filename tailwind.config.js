9a2cdf0645221c78a0c436b74634af4287b04de2
Wizzel1 <r_christian@gmx.de>
2023-12-18 13:07:01 +0100

    Change default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-    minWidth: {
-      '1/2': '50%',
-    }
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+      fontFamily: {
+        sans: ['Noto Sans', ...defaultTheme.fontFamily.sans]
+      }
+		},
+		minWidth: {
+			'1/2': '50%'
+		}
+	},
+	plugins: []
+};
