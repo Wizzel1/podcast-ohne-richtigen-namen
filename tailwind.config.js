dd5cf1953ed28c109555b6d799fd3de1c47d04e7
Wizzel1 <r_christian@gmx.de>
2023-12-20 13:17:13 +0100

    Add preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/preline/preline.js'],
 	theme: {
 		extend: {
-      fontFamily: {
-        sans: ['Noto Sans', ...defaultTheme.fontFamily.sans]
-      }
+			fontFamily: {
+				sans: ['Noto Sans', ...defaultTheme.fontFamily.sans]
+			}
 		},
 		minWidth: {
 			'1/2': '50%'
 		}
 	},
-	plugins: []
+	plugins: [require('preline/plugin')]
 };
