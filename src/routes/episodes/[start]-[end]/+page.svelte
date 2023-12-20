a001a348bab9fe2b85e7c4d81070e7f2ff299a4e
Wizzel1 <r_christian@gmx.de>
2023-12-20 13:17:29 +0100

    Update episode range component

--- src/routes/episodes/[start]-[end]/+page.svelte
+++ src/routes/episodes/[start]-[end]/+page.svelte
@@ -4,9 +4,45 @@
 	$: ({ episodes } = data);
 </script>
 
-<ul role="list">
+<div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 2xl:grid-cols-5 gap-4">
 	{#each episodes as item}
-		<li>
+		<div class="flex flex-col justify-between border-2 shadow-sm rounded-xl p-4 md:p-5 border-gray-500">
+			<h3 class="text-lg font-bold text-gray-800 dark:text-white">
+				#{item.number} |
+				{item.title}
+			</h3>
+			<!-- <p class="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
+		  Card subtitle
+		</p> -->
+			{#if item.questions}
+				{#each item.questions as question}
+					<!-- <p class="text-sm font-normal mt-2">{question.question}</p> -->
+					<p class="mt-2 text-gray-500 dark:text-gray-400">
+						{question.question}
+					</p>
+				{/each}
+			{/if}
+			<a
+				class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+				href="/episodes/{item.number}"
+			>
+				Zur Folge
+				<svg
+					class="flex-shrink-0 w-4 h-4"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg
+				>
+			</a>
+		</div>
+
+		<!-- <li>
 			<a
 				class="block p-4 m-5 border-2 border-solid rounded-md hover:bg-sky-700 hover:cursor-pointer hover:no-underline"
 				href="/episodes/{item.number}"
@@ -18,6 +54,6 @@
 					{/each}
 				{/if}
 			</a>
-		</li>
+		</li> -->
 	{/each}
-</ul>
+</div>
