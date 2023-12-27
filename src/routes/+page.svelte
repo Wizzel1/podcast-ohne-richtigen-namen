907c52f99c830c5af88629322548f1621e42a99c
Wizzel1 <r_christian@gmx.de>
2023-12-27 20:51:29 +0100

    Update home spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <div class="flex flex-col justify-center items-center">
-	<h1 class="text-2xl max-w-[50%] mb-8">Podcast ohne richtigen Namen</h1>
-	<p class="max-w-[50%]">
+	<h1 class="text-2xl max-w-[80%] lg:max-w-[50%] mb-8">Podcast ohne richtigen Namen</h1>
+	<p class="max-w-[80%] lg:max-w-[50%] mb-8">
 		Hier findet ihr die Rätsel aus allen Folgen Podcast ohne (richtigen) Namen (anfangs von George
 		noch als „wer googelt kriegt aufs Maul“ angekündigt). Zusätzlich zu den Fragen und Antworten
 		(mit Link zur Quelle) könnt ihr sehen, ob das Rätsel gelöst wurde und wenn ja, von wem. Einen
-		Überblick findet ihr auf der Rangliste. Viel Spaß beim Nachlesen.
+		Überblick findet ihr auf der Rangliste.
 	</p>
+	<p class="max-w-[80%] lg:max-w-[50%]">Viel Spaß beim Nachlesen.</p>
 </div>
